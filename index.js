f34ec6c00a35d5a98380c9ca2f75a9f3a234a0a4
Molly Todd <mollyjt89@gmail.com>
2023-06-26 22:22:27 -0700

    7 of 9 passing, stuck on two

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distanceInFeet)
+{ 
+  
+   if(distanceInFeet<=400) return "This one is on me!"
+  else if (distanceInFeet>400 && distanceInFeet <=2000)
+ return "That will be twenty bucks."
+  else if (distanceInFeet>2000) 
+ return "I will gladly take your thirty bucks."
+else if (distanceInFeet > 2000 && distanceInFeet <= 2500) 
+  return "No can do.";
+ 
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? "Ok, sounds good." : "No go.";
+  
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+ternaryCheckCity('NYC');
+
+function switchOnCharmFromTip(tip) {
+  if (tip === 'generous')
+  return "Thank you so much.";
+   else return "Bye.";
 }
+
+
+  
+
+  
+switchOnCharmFromTip('generous');
