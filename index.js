37ca5706964949e2450786635608e6ff519b3ded
Molly Todd <mollyjt89@gmail.com>
2023-07-13 18:46:21 -0700

    redid lab again

--- index.js
+++ index.js
@@ -1,31 +1,26 @@
-function scuberGreetingForFeet(distanceInFeet)
-{ 
-  
-   if(distanceInFeet<=400) return "This one is on me!"
-  else if (distanceInFeet>400 && distanceInFeet <=2000)
- return "That will be twenty bucks."
-  else if (distanceInFeet>2000) 
- return "I will gladly take your thirty bucks."
-else if (distanceInFeet > 2000 && distanceInFeet <= 2500) 
-  return "No can do.";
- 
+function scuberGreetingForFeet(distanceInFeet) {;
+  if (distanceInFeet <= 400) {
+    return "This one is on me!";
+  } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {;
+    return "That will be twenty bucks.";
+  } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {;
+    return "I will gladly take your thirty bucks.";
+  } else if (distanceInFeet > 2500) {;
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(city) {
-  return city === 'NYC' ? "Ok, sounds good." : "No go.";
-  
+function ternaryCheckCity(city) {;
+  return city === ('NYC') ? ("Ok, sounds good.") : ("No go.");
 }
 
-ternaryCheckCity('NYC');
-
-function switchOnCharmFromTip(tip) {
-  if (tip === 'generous')
+function switchOnCharmFromTip(tipSize) {;
+ if (tipSize === 'generous') {;
   return "Thank you so much.";
-   else return "Bye.";
 }
-
-
-  
-
-  
-switchOnCharmFromTip('generous');
+else if (tipSize === 'not as generous') {
+  return "Thank you."
+}
+else
+  return "Bye.";
+}
