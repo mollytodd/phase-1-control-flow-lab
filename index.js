5927ce9ba490acf2f353ae21b6632f1ffb0abc40
Molly Todd <mollyjt89@gmail.com>
2023-07-31 10:49:53 -0700

    did labs again for 7.31

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
-function scuberGreetingForFeet(distanceInFeet) {;
-  if (distanceInFeet <= 400) {
-    return "This one is on me!";
-  } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {;
+function scuberGreetingForFeet(distance) {
+  if (distance <= 400) return "This one is on me!";
+  else if (distance > 400 && distance < 2000)
     return "That will be twenty bucks.";
-  } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {;
+  else if (distance > 2000 && distance < 2500)
     return "I will gladly take your thirty bucks.";
-  } else if (distanceInFeet > 2500) {;
-    return "No can do.";
+  else if (distance >= 2500) return "No can do.";
+}
+
+console.log(scuberGreetingForFeet(2500));
+//
+function ternaryCheckCity(cityName) {
+  return cityName === "NYC" ? "Ok, sounds good." : "No go.";
+}
+
+console.log(ternaryCheckCity("NYC"));
+//
+
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+  default:
+      return "Bye.";
   }
 }
-
-function ternaryCheckCity(city) {;
-  return city === ('NYC') ? ("Ok, sounds good.") : ("No go.");
-}
-
-function switchOnCharmFromTip(tipSize) {;
- if (tipSize === 'generous') {;
-  return "Thank you so much.";
-}
-else if (tipSize === 'not as generous') {
-  return "Thank you."
-}
-else
-  return "Bye.";
-}
+console.log(switchOnCharmFromTip("generous"));
